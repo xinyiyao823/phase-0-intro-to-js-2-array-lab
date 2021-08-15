0326c764d491f89e13fa4d086acebdbf803efe2b
Xinyi <xinyiyao84@yahoo.com>
2021-08-15 00:03:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1, cats.length);
+}
